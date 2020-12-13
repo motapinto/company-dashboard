4d598adc51091c1a422194795d59d25237a8b874
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-12-13 15:44:50 +0000

    Fix revenue and expenses

--- api/src/modules/profitAndLoss.js
+++ api/src/modules/profitAndLoss.js
@@ -12,27 +12,18 @@ export default (server, db) => {
 
         if(debitLine && debitLine instanceof Array) {
           for(const debit of debitLine) {
-            if(isRevenueTaxonomy(getAccount(debit.AccountID))) {
-              revenue += debit.DebitAmount;
-            }        
+            revenue = updateRevenueTaxonomy(revenue, getAccount(debit.AccountID), debit.DebitAmount);        
           }
         } else {
-          if(isRevenueTaxonomy(getAccount(debitLine.AccountID))) {
-            revenue += debitLine.DebitAmount;
-          }      
+          revenue = updateRevenueTaxonomy(revenue, getAccount(debitLine.AccountID), debitLine.DebitAmount);  
         }
 
         if(creditLine && creditLine instanceof Array) {
           for(const credit of creditLine) {
-            if(isExpenseTaxonomy(getAccount(credit.AccountID))) {
-              expenses += credit.CreditAmount;
-            }     
+            expenses = updateExpensesTaxonomy(expenses, getAccount(credit.AccountID), credit.CreditAmount)  
           }
         } else {
-
-          if(isExpenseTaxonomy(getAccount(creditLine.AccountID))) {
-            expenses += creditLine.CreditAmount;
-          }  
+          expenses = updateExpensesTaxonomy(expenses, getAccount(creditLine.AccountID), creditLine.CreditAmount)   
         }
       };
     };
@@ -47,80 +38,80 @@ export default (server, db) => {
       account => account.AccountID === accountID )[0];
   };
 
-  const isRevenueTaxonomy = (account) => {
+  const updateRevenueTaxonomy = (revenue, account, amount) => {
     const taxonomyCode = account.TaxonomyCode;
 
     switch(true) {
       /* Vendas e serviços prestados */
-      case taxonomyCode >= 506 && taxonomyCode <= 518: return true;
+      case taxonomyCode >= 506 && taxonomyCode <= 518: return revenue + amount;
+      case taxonomyCode == 511 || taxonomyCode == 518: return revenue - amount;
 
       /* Subsídios à exploração */
-      case taxonomyCode >= 527 && taxonomyCode <= 528: return true;
+      case taxonomyCode >= 527 && taxonomyCode <= 528: return revenue + amount;
       
       /* Ganhos / perdas imputados de subsidiárias, associadas e empreendimentos conjuntos */
-      case taxonomyCode >= 614 && taxonomyCode <= 616: return true;
-      case taxonomyCode >= 638 && taxonomyCode <= 639: return true;
-      case taxonomyCode >= 479 && taxonomyCode <= 482: return true;
+      case taxonomyCode >= 614 && taxonomyCode <= 616: return revenue + amount;
+      case taxonomyCode >= 638 && taxonomyCode <= 639: return revenue + amount;
+      case taxonomyCode >= 479 && taxonomyCode <= 482: return revenue - amount;
 
       /* Variação nos inventários da produção */
-      case taxonomyCode >= 519 && taxonomyCode <= 522: return true;
+      case taxonomyCode >= 519 && taxonomyCode <= 522: return revenue + amount;
 
       /* Trabalhos para a própria entidade */
-      case taxonomyCode >= 523 && taxonomyCode <= 526: return true;
+      case taxonomyCode >= 523 && taxonomyCode <= 526: return revenue + amount;
 
       /* Aumentos / reduções de justo valor */
-      case taxonomyCode >= 594 && taxonomyCode <= 602: return true;
-      case taxonomyCode >= 454 && taxonomyCode <= 462: return true;
+      case taxonomyCode >= 594 && taxonomyCode <= 602: return revenue + amount;
+      case taxonomyCode >= 454 && taxonomyCode <= 462: return revenue - amount;
 
       /* Outros rendimentos  */
-      case taxonomyCode >= 603 && taxonomyCode <= 613: return true;
-      case taxonomyCode >= 617 && taxonomyCode <= 634: return true;
-      case taxonomyCode >= 636 && taxonomyCode <= 637: return true;
-      case taxonomyCode == 640: return true;
-      case taxonomyCode == 642: return true;
+      case taxonomyCode >= 603 && taxonomyCode <= 613: return revenue + amount;
+      case taxonomyCode >= 617 && taxonomyCode <= 634: return revenue + amount;
+      case taxonomyCode >= 636 && taxonomyCode <= 637: return revenue + amount;
+      case taxonomyCode == 640: return revenue + amount;
+      case taxonomyCode == 642: return revenue + amount;
   
-      default: false;
+      default: return revenue;
     }
   }
 
-  const isExpenseTaxonomy = (account) => {
-    console.log(account);
+  const updateExpensesTaxonomy = (expenses, account, amount) => {
     const taxonomyCode = account.TaxonomyCode;
 
     switch(true) {
       /* Custo das mercadorias vendidas e das matérias consumidas */
-      case taxonomyCode >= 353 && taxonomyCode <= 355: return true;
+      case taxonomyCode >= 353 && taxonomyCode <= 355: return expenses + amount;
 
       /* Fornecimentos e serviços externos */
-      case taxonomyCode >= 356 && taxonomyCode <= 384: return true;
+      case taxonomyCode >= 356 && taxonomyCode <= 384: return expenses + amount;
       
       /* Gastos com o pessoal */
-      case taxonomyCode >= 385 && taxonomyCode <= 393: return true;
+      case taxonomyCode >= 385 && taxonomyCode <= 393: return expenses + amount;
 
       /* Imparidade / ajustamentos de inventários (perdas / reversões)  */
-      case taxonomyCode >= 415 && taxonomyCode <= 421: return true;
-      case taxonomyCode >= 549 && taxonomyCode <= 555: return true;
+      case taxonomyCode >= 415 && taxonomyCode <= 421: return expenses + amount;
+      case taxonomyCode >= 549 && taxonomyCode <= 555: return expenses - amount;
 
       /* Imparidade de dívidas a receber (perdas / reversões) */
-      case taxonomyCode >= 413 && taxonomyCode <= 414: return true;
-      case taxonomyCode >= 547 && taxonomyCode <= 548: return true;
+      case taxonomyCode >= 413 && taxonomyCode <= 414: return expenses + amount;
+      case taxonomyCode >= 547 && taxonomyCode <= 548: return expenses - amount;
 
       /* Provisões (aumentos / reduções)  */
-      case taxonomyCode >= 463 && taxonomyCode <= 470: return true;
-      case taxonomyCode >= 586 && taxonomyCode <= 593: return true;
+      case taxonomyCode >= 463 && taxonomyCode <= 470: return expenses + amount;
+      case taxonomyCode >= 586 && taxonomyCode <= 593: return expenses - amount;
 
       /* Imparidade de investimentos não depreciáveis / amortizáveis (perdas / reversões) */
-      case taxonomyCode == 412: return true;
-      case taxonomyCode >= 422 && taxonomyCode <= 425: return true;
-      case taxonomyCode >= 441 && taxonomyCode <= 453: return true;
-      case taxonomyCode >= 556 && taxonomyCode <= 558: return true;
-      case taxonomyCode >= 573 && taxonomyCode <= 585: return true;
+      case taxonomyCode == 412: return expenses + amount;
+      case taxonomyCode >= 422 && taxonomyCode <= 425: return expenses + amount;
+      case taxonomyCode >= 441 && taxonomyCode <= 453: return expenses + amount;
+      case taxonomyCode >= 556 && taxonomyCode <= 558: return expenses - amount;
+      case taxonomyCode >= 573 && taxonomyCode <= 585: return expenses + amount;
 
       /* Outros gastos */
-      case taxonomyCode >= 471 && taxonomyCode <= 478: return true;
-      case taxonomyCode >= 483 && taxonomyCode <= 499: return true;
+      case taxonomyCode >= 471 && taxonomyCode <= 478: return expenses + amount;
+      case taxonomyCode >= 483 && taxonomyCode <= 499: return expenses + amount;
 
-      default: false;
+      default: return expenses;
     }
   }
 }     
